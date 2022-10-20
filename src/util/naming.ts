a42b9341d6fb47011a2a3d8b49989f980964faea
Matthew A. Miller <linuxwolf@outer-planes.net>
2022-10-19 19:37:40 -0600

    be more specific for naming functions

--- src/util/naming.ts
+++ src/util/naming.ts
@@ -11,13 +11,13 @@ export class InvalidNameError extends ErrBase {
   }
 }
 
-export function validate(name: string, allowEmpty = false): boolean {
+export function validateName(name: string, allowEmpty = false): boolean {
   if (!name && allowEmpty) return true;
   return NAME_PTN.test(name);
 }
 
-export function check(name: string, allowEmpty = false): string {
-  if (!validate(name, allowEmpty)) {
+export function checkName(name: string, allowEmpty = false): string {
+  if (!validateName(name, allowEmpty)) {
     throw new InvalidNameError(name);
   }
   return name;
--- test/util/naming.test.ts
+++ test/util/naming.test.ts
@@ -1,20 +1,20 @@
 import { describe, expect, it } from "../deps.ts";
 
-import { check, InvalidNameError, validate } from "../../src/util/naming.ts";
+import { checkName, InvalidNameError, validateName } from "../../src/util/naming.ts";
 
 describe("util/naming", () => {
-  describe("validate()", () => {
+  describe("validateName()", () => {
     describe("successes", () => {
       it("supports a simple name", () => {
-        const result = validate("simple");
+        const result = validateName("simple");
         expect(result).to.be.true;
       });
       it("supports a name with digits", () => {
-        const result = validate("simple42");
+        const result = validateName("simple42");
         expect(result).to.be.true;
       });
       it("supports just digits", () => {
-        const result = validate("20221017");
+        const result = validateName("20221017");
         expect(result).to.be.true;
       });
       it("supports some symbols", () => {
@@ -26,22 +26,22 @@ describe("util/naming", () => {
           "test$task",
         ];
         for (const name of names) {
-          const result = validate(name);
+          const result = validateName(name);
           expect(result, `validating ${name}`).to.be.true;
         }
       });
       it("supports a full mix", () => {
-        const result = validate("test_100.this-is");
+        const result = validateName("test_100.this-is");
         expect(result).to.be.true;
       });
     });
     describe("edge successes", () => {
       it("supports just a letter", () => {
-        const result = validate("a");
+        const result = validateName("a");
         expect(result).to.be.true;
       });
       it("supports just a digit", () => {
-        const result = validate("3");
+        const result = validateName("3");
         expect(result).to.be.true;
       });
       it("supports a leading symbol", () => {
@@ -53,7 +53,7 @@ describe("util/naming", () => {
           "$testtask",
         ];
         for (const name of names) {
-          const result = validate(name);
+          const result = validateName(name);
           expect(result, `validating ${name}`).to.be.true;
         }
       });
@@ -66,7 +66,7 @@ describe("util/naming", () => {
           "testtask$",
         ];
         for (const name of names) {
-          const result = validate(name);
+          const result = validateName(name);
           expect(result, `validating ${name}`).to.be.true;
         }
       });
@@ -74,43 +74,43 @@ describe("util/naming", () => {
 
     describe("failures", () => {
       it("falis on spaces", () => {
-        const result = validate("invalid name");
+        const result = validateName("invalid name");
         expect(result).to.be.false;
       });
       it("fails on leading spaces", () => {
-        const result = validate("  invalid-name");
+        const result = validateName("  invalid-name");
         expect(result).to.be.false;
       });
       it("fails on too many symbols", () => {
-        const result = validate("$$");
+        const result = validateName("$$");
         expect(result).to.be.false;
       });
     });
 
     describe("empty", () => {
       it("fails if empty", () => {
-        const result = validate("");
+        const result = validateName("");
         expect(result).to.be.false;
       });
       it("succeeds if allowEmpty is true", () => {
-        const result = validate("", true);
+        const result = validateName("", true);
         expect(result).to.be.true;
       });
     });
   });
 
-  describe("check()", () => {
+  describe("validateName()", () => {
     describe("successes", () => {
       it("supports a simple name", () => {
-        const result = check("simple");
+        const result = checkName("simple");
         expect(result).to.equal("simple");
       });
       it("supports a name with digits", () => {
-        const result = check("simple42");
+        const result = checkName("simple42");
         expect(result).to.equal("simple42");
       });
       it("supports just digits", () => {
-        const result = check("20221017");
+        const result = checkName("20221017");
         expect(result).to.equal("20221017");
       });
       it("supports some symbols", () => {
@@ -122,22 +122,22 @@ describe("util/naming", () => {
           "test$task",
         ];
         for (const name of names) {
-          const result = check(name);
+          const result = checkName(name);
           expect(result, `checking ${name}`).to.equal(name);
         }
       });
       it("supports a full mix", () => {
-        const result = check("test_100.this-is");
+        const result = checkName("test_100.this-is");
         expect(result).to.equal("test_100.this-is");
       });
     });
     describe("edge successes", () => {
       it("supports just a letter", () => {
-        const result = check("a");
+        const result = checkName("a");
         expect(result).to.equal("a");
       });
       it("supports just a digit", () => {
-        const result = check("3");
+        const result = checkName("3");
         expect(result).to.equal("3");
       });
       it("supports a leading symbol", () => {
@@ -149,7 +149,7 @@ describe("util/naming", () => {
           "$testtask",
         ];
         for (const name of names) {
-          const result = check(name);
+          const result = checkName(name);
           expect(result, `checking ${name}`).to.equal(name);
         }
       });
@@ -162,7 +162,7 @@ describe("util/naming", () => {
           "testtask$",
         ];
         for (const name of names) {
-          const result = check(name);
+          const result = checkName(name);
           expect(result, `checking ${name}`).to.equal(name);
         }
       });
@@ -170,17 +170,17 @@ describe("util/naming", () => {
 
     describe("failures", () => {
       it("falis on spaces", () => {
-        expect(() => check("invalid name"))
+        expect(() => checkName("invalid name"))
           .to.throw(InvalidNameError)
           .and.have.property("value", "invalid name");
       });
       it("fails on leading spaces", () => {
-        expect(() => check("  invalid-name"))
+        expect(() => checkName("  invalid-name"))
           .to.throw(InvalidNameError)
           .and.have.property("value", "  invalid-name");
       });
       it("fails on too many symbols", () => {
-        expect(() => check("$$"))
+        expect(() => checkName("$$"))
           .to.throw(InvalidNameError)
           .and.have.property("value", "$$");
       });
@@ -188,12 +188,12 @@ describe("util/naming", () => {
 
     describe("empty", () => {
       it("fails if empty", () => {
-        expect(() => check(""))
+        expect(() => checkName(""))
           .to.throw(InvalidNameError)
           .and.have.property("value", "");
       });
       it("succeeds if allowEmpty is true", () => {
-        const result = check("", true);
+        const result = checkName("", true);
         expect(result).to.equal("");
       });
     });
