a53e7fd4dca957b3c3e477b307578fb630c9cea2
Matthew A. Miller <linuxwolf@outer-planes.net>
2022-12-28 18:06:02 -0700

    resolver for projects

---
+++ src/core/resolver.ts
@@ -0,0 +1,142 @@
+import { path } from "../deps.ts";
+import { normalize } from "../util/normalize.ts";
+import { Optional } from "../util/types.ts";
+
+import { Parser } from "../parser/yaml.ts";
+import { Project, ProjectBuilder } from "./project.ts";
+import { TargetPath, TargetPathType } from "./target.ts";
+import * as errors from "../errors/mod.ts";
+import log from "../log.ts";
+
+export class ProjectLoader {
+  readonly cache: Record<string, Project> = {};
+
+  parse(filepath: string): Promise<ProjectBuilder> {
+    return (new Parser()).load(filepath);
+  }
+  
+  async load(dir: string): Promise<ProjectBuilder> {
+    dir = normalize(dir);
+
+    let project: Optional<ProjectBuilder> = undefined;
+    let curr = dir;
+    do {
+      try {
+        log.debug(`loading project from ${curr}...`);
+        project = await this.parse(curr);
+      } catch (err) {
+        log.debug(`failed to load project from ${curr}: ${err.message}`);
+        const parent = path.dirname(curr);
+        if (parent === curr) {
+          throw new errors.ConfigMissing(dir);
+        }
+        curr = parent;
+      }
+    } while (!project);
+
+    return project;
+  }
+
+  async build(dir: string, root: Optional<Project> = undefined): Promise<Project> {
+    dir = normalize(dir);
+
+    let project = this.cache[dir];
+    if (project) {
+      return project;
+    }
+
+    const builder = await this.load(dir);
+    if (!builder.root) {
+      try {
+        project = await this.build(path.dirname(dir), root);
+      } catch (e) {
+        log.debug(`failed to build project: ${e.message}`);
+        // treat latest builder as root
+        builder.asRoot();
+      }
+    }
+    project = builder.build(project);
+    this.cache[project.filepath] = project;
+
+    return project;
+  }
+}
+
+export class ResolverContext {
+  private loader: ProjectLoader;
+  readonly current: Project;
+  readonly root: Project;
+
+  constructor(loader: ProjectLoader, project: Project) {
+    this.loader = loader;
+    this.current = project;
+    this.root = project.rootProject;
+  }
+
+  targetPath(filepath: string): TargetPath {
+    const tpath = new TargetPath(filepath);
+    let base: TargetPath;
+    switch (tpath.type) {
+      case TargetPathType.Root:
+        base = new TargetPath(this.root.filepath);
+        break;
+      case TargetPathType.Relative:
+        base = new TargetPath(this.current.filepath);
+        break;
+      case TargetPathType.Absolute:
+        // verify path is a sub-path of the root
+        if (path.common([
+          this.root.filepath,
+          tpath.path,
+        ]) !== (this.root.filepath + "/")) {
+          throw new errors.InvalidPath(filepath, "absolute path not allowed");
+        }
+        return tpath;
+    }
+
+    return tpath.relativeTo(base);
+  }
+
+  async resolve(filepath: string): Promise<Project> {
+    const tpath = this.targetPath(filepath).path;
+    switch (tpath) {
+      case this.current.filepath:
+        return this.current;
+      case this.root.filepath:
+        return this.root;
+      default:
+        return await this.loader.build(tpath);
+    }
+  }
+}
+
+export class Resolver {
+  readonly root: Project;
+  private readonly loader: ProjectLoader;
+
+  private constructor(root: Project, loader: ProjectLoader) {
+    this.root = root;
+    this.loader = loader;
+  }
+
+  static async create(dir: string): Promise<Resolver> {
+    // ensure dir starts as an absolute path
+    dir = path.resolve(dir);
+
+    const loader = new ProjectLoader();
+    const prj = await loader.build(dir);
+    const root = prj.rootProject;
+
+    return new Resolver(root, loader);
+  }
+
+  within(project: Project): Promise<ResolverContext>;
+  within(filepath: string): Promise<ResolverContext>;
+  async within(prjOrPath: Project | string): Promise<ResolverContext> {
+    const project = (typeof prjOrPath === "string") ?
+                    await this.loader.build(prjOrPath) :
+                    prjOrPath as Project;
+
+    return new ResolverContext(this.loader, project);
+  }
+}
---
+++ test/core/resolver.test.ts
@@ -0,0 +1,301 @@
+import { afterEach, beforeEach, describe, expect, it, sinon } from "../deps.ts";
+
+import { Parser } from "../../src/parser/yaml.ts";
+
+import { ProjectLoader, Resolver, ResolverContext } from "../../src/core/resolver.ts";
+import { Project, ProjectBuilder } from "../../src/core/project.ts";
+import { ConfigMissing, InvalidPath } from "../../src/errors/mod.ts";
+import { TargetPath } from "../../src/core/target.ts";
+
+describe("core/resolver", () => {
+  describe("ProjectLoader", () => {
+    let loader: ProjectLoader;
+
+    beforeEach(() => {
+      loader = new ProjectLoader();
+    });
+
+    describe(".parse()", () => {
+      const builder = new ProjectBuilder({
+        filepath: "/usr/local/src",
+        root: true,
+      });
+      let loadStub: sinon.SinonStub;
+
+      beforeEach(() => {
+        loadStub = sinon.stub(Parser.prototype, "load");
+      });
+      afterEach(() => {
+        loadStub.restore();
+      });
+
+      it("results in a ProjectBuilder", async () => {
+        loadStub.resolves(builder);
+
+        const result = await loader.parse("/usr/local/src");
+        expect(result).to.equal(builder);
+        expect(loadStub).to.have.callCount(1);
+        expect(loadStub).to.be.calledWith("/usr/local/src");
+      });
+    });
+    describe(".load()", () => {
+      const builder = new ProjectBuilder({
+        filepath: "/usr/local/src",
+        root: true,
+      });
+      let parseStub: sinon.SinonStub;
+
+      beforeEach(() => {
+        parseStub = sinon.stub(loader, "parse");
+      });
+      afterEach(() => {
+        parseStub.restore();
+      });
+
+      it("loads a project", async () => {
+        parseStub.resolves(builder);
+        const result = await loader.load("/usr/local/src");
+        expect(result).to.equal(builder);
+        expect(parseStub).to.have.callCount(1);
+        expect(parseStub).to.be.calledWith("/usr/local/src");
+      });
+      it("walks directories 'up' to a project", async () => {
+        parseStub.withArgs("/usr/local/src/sub").
+                  rejects(new ConfigMissing("/usr/local/src/sub"));
+        parseStub.withArgs("/usr/local/src").
+                  resolves(builder);
+        
+        const result = await loader.load("/usr/local/src/sub");
+        expect(result).to.equal(builder);
+        expect(parseStub).to.have.callCount(2);
+        expect(parseStub).to.be.calledWith("/usr/local/src/sub");
+        expect(parseStub).to.be.calledWith("/usr/local/src");
+      });
+      it("fails if no project found", async () => {
+        parseStub.rejects(new Deno.errors.NotFound());
+
+        const result = loader.load("/usr/local/src/sub");
+        await expect(result).to.be.rejectedWith(ConfigMissing).
+                             to.eventually.have.property("filepath", "/usr/local/src/sub");
+        expect(parseStub).to.have.callCount(5);
+        expect(parseStub).to.be.calledWith("/usr/local/src/sub");
+        expect(parseStub).to.be.calledWith("/usr/local/src");
+        expect(parseStub).to.be.calledWith("/usr/local");
+        expect(parseStub).to.be.calledWith("/usr");
+        expect(parseStub).to.be.calledWith("/");
+      });
+    });
+    describe(".build()", () => {
+      let subBuilder: ProjectBuilder;
+      let rootBuilder: ProjectBuilder;
+      let loadStub: sinon.SinonStub;
+
+      beforeEach(() => {
+        subBuilder = new ProjectBuilder("/usr/local/src/sub");
+        rootBuilder = new ProjectBuilder("/usr/local/src").asRoot(),
+
+        loadStub = sinon.stub(loader, "load");
+        loadStub.withArgs("/usr/local/src").resolves(rootBuilder);
+        loadStub.withArgs("/usr/local/src/sub").resolves(subBuilder);
+      });
+      afterEach(() => {
+        loadStub.restore();
+      });
+
+      it("it builds the root project", async () => {
+        const root = rootBuilder.build();
+        const result = await loader.build("/usr/local/src");
+        expect(result).to.deep.equal(root);
+        expect(loadStub).to.have.callCount(1);
+        expect(loadStub).to.be.calledWith("/usr/local/src");
+      });
+
+      it("it builds the sub and root projects", async () => {
+        const root = rootBuilder.build();
+        const sub = subBuilder.build(root);
+
+        const result = await loader.build("/usr/local/src/sub");
+        expect(result).to.deep.equal(sub);
+        expect(loadStub).to.have.callCount(2);
+        expect(loadStub).to.be.calledWith("/usr/local/src/sub");
+        expect(loadStub).to.be.calledWith("/usr/local/src");
+      });
+
+      it("makes last successfully loaded project the root", async () => {
+        const sub = new ProjectBuilder({...subBuilder}).asRoot().build();
+        loadStub.withArgs("/usr/local/src").rejects(new ConfigMissing("/usr/local/src"));
+
+        const result = await loader.build("/usr/local/src/sub");
+        expect(result).to.deep.equal(sub);
+        expect(loadStub).to.have.callCount(2);
+        expect(loadStub).to.be.calledWith("/usr/local/src/sub");
+        expect(loadStub).to.be.calledWith("/usr/local/src");
+      });
+
+      it("returns cached projects", async () => {
+        const root = rootBuilder.build();
+        const sub = subBuilder.build(root);
+
+        let result: Project;
+        result = await loader.build("/usr/local/src/sub");
+        expect(result).to.deep.equal(sub);
+
+        const cachedSub = result;
+        const cachedRoot = cachedSub.parent;
+        result = await loader.build("/usr/local/src/sub");
+        expect(result).to.equal(cachedSub);
+        result = await loader.build("/usr/local/src");
+        expect(result).to.equal(cachedRoot);
+
+        expect(loadStub).to.have.callCount(2);
+        expect(loadStub).to.be.calledWith("/usr/local/src/sub");
+        expect(loadStub).to.be.calledWith("/usr/local/src");
+      });
+    });
+  });
+
+  describe("ResolverContext", () => {
+    const root = new ProjectBuilder("/usr/local/src").asRoot().build();
+    const sub1 = new ProjectBuilder("/usr/local/src/sub1").build(root);
+    const sub1_1 = new ProjectBuilder("/usr/local/src/sub1/subsub1").build(sub1);
+    const sub2 = new ProjectBuilder("/usr/local/src/sub2").build(root);
+
+    let loader: ProjectLoader;
+    let buildStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      loader = new ProjectLoader();
+      buildStub = sinon.stub(loader, "build");
+      buildStub.withArgs("/usr/local/src").resolves(root);
+      buildStub.withArgs("/usr/local/src/sub1").resolves(sub1);
+      buildStub.withArgs("/usr/local/src/sub1/subsub1").resolves(sub1_1);
+      buildStub.withArgs("/usr/local/src/sub2").resolves(sub2);
+    });
+
+    describe(".targetPath()", () => {
+      const ctx = new ResolverContext(loader, sub1);
+
+      it("resolves a relative path based on the current project", () => {
+        let result: TargetPath;
+
+        result = ctx.targetPath("test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1:test-task"));
+
+        result = ctx.targetPath("./subsub1:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1/subsub1:test-task"));
+
+        result = ctx.targetPath("../sub2:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub2:test-task"));
+      });
+      it("resolves a rooted path", () => {
+        let result: TargetPath;
+
+        result = ctx.targetPath("//:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src:test-task"));
+
+        result = ctx.targetPath("//sub1:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1:test-task"));
+
+        result = ctx.targetPath("//sub1/subsub1:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1/subsub1:test-task"));
+
+        result = ctx.targetPath("//sub2:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub2:test-task"));
+      });
+      it("resolves an absolute path within the root project filepath", () => {
+        let result: TargetPath;
+
+        result = ctx.targetPath("/usr/local/src/sub1:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1:test-task"));
+
+        result = ctx.targetPath("/usr/local/src:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src:test-task"));
+
+        result = ctx.targetPath("/usr/local/src/sub1/subsub1:test-task");
+        expect(result).to.deep.equal(new TargetPath("/usr/local/src/sub1/subsub1:test-task"));
+      });
+      it("fails if absolute path not within root project path", () => {
+        expect(() => { ctx.targetPath("/usr/local/share/project:test-target"); }).
+            to.throw(InvalidPath).
+            to.have.property("filepath", "/usr/local/share/project:test-target");
+      });
+    });
+
+    describe(".resolve()", () => {
+      let ctx: ResolverContext;
+
+      beforeEach(() => {
+        ctx = new ResolverContext(loader, sub1);
+      });
+
+      it("loads the requested project", async () => {
+        const result = await ctx.resolve("/usr/local/src/sub1/subsub1");
+        expect(result).to.equal(sub1_1);
+      });
+
+      it("returns the current project for its path", async () => {
+        const result = await ctx.resolve(ctx.current.filepath);
+        expect(result).to.equal(ctx.current);
+      });
+
+      it("returns the root project for its path", async () => {
+        const result = await ctx.resolve(root.filepath);
+        expect(result).to.equal(root);
+      });
+    });
+  });
+
+  describe("Resolver", () => {
+    const root = new ProjectBuilder("/usr/local/src").asRoot().build();
+    const sub1 = new ProjectBuilder("/usr/local/src/sub1").build(root);
+    const sub1_1 = new ProjectBuilder("/usr/local/src/sub1/subsub1").build(sub1);
+    const sub2 = new ProjectBuilder("/usr/local/src/sub2").build(root);
+
+    let buildStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      buildStub = sinon.stub(ProjectLoader.prototype, "build");
+      buildStub.withArgs("/usr/local/src").resolves(root);
+      buildStub.withArgs("/usr/local/src/sub1").resolves(sub1);
+      buildStub.withArgs("/usr/local/src/sub1/subsub1").resolves(sub1_1);
+      buildStub.withArgs("/usr/local/src/sub2").resolves(sub2);
+    });
+
+    afterEach(() => {
+      buildStub.restore();
+    });
+
+    describe(".create()", () => {
+      it("creates a Resolver from the project root directory", async () => {
+        const result = await Resolver.create("/usr/local/src");
+        expect(result.root).to.equal(root);
+      });
+      it("creates a Resolver from a sub-directory", async () => {
+        const result = await Resolver.create("/usr/local/src/sub1");
+        expect(result.root).to.equal(root);
+      });
+    });
+
+    describe(".within()", () => {
+      let resolver: Resolver;
+
+      beforeEach(async () => {
+        resolver = await Resolver.create("/usr/local/src");
+        buildStub.resetHistory();
+      });
+
+      it("creates a context from the given filepath", async () => {
+        const result = await resolver.within("/usr/local/src/sub1");
+        expect(result.current).to.equal(sub1);
+        expect(result.root).to.equal(root);
+        expect(buildStub).to.be.calledWith("/usr/local/src/sub1");
+      });
+      it("creats a context from the given Project", async () => {
+        const result = await resolver.within(sub2);
+        expect(result.current).to.equal(sub2);
+        expect(result.root).to.equal(root);
+        expect(buildStub).to.have.not.been.called;
+      });
+    });
+  });
+});
