a1bc6e4cc643f35df647fab611f9fc8e9e671d64
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-02 14:32:46 -0700

    with interpolation

--- src/vars.ts
+++ src/vars.ts
@@ -9,7 +9,7 @@ export interface VariablesContext {
   readonly vars: Variables;
 }
 
-const RE_VARNAME=/^[_a-zA-Z][_a-zA-Z0-9]*$/;
+const RE_VARNAME = /^[_a-zA-Z][_a-zA-Z0-9]*$/;
 
 function validate(name: string): string {
   RE_VARNAME.lastIndex = 0;
@@ -18,6 +18,17 @@ function validate(name: string): string {
   }
 
   return name;
+}
+
+const RE_INTERPOLATION = /\$(?:\$|\{([_a-zA-Z][_a-zA-Z0-9]*)(?:\:-(.*))?\})/g;
+
+function interpolate(value: string, envs: Variables): string {
+  return value.replaceAll(RE_INTERPOLATION, (match, name, defaultVal) => {
+    if (match === "$$") {
+      return "$";
+    }
+    return envs[name] ?? defaultVal ?? "";
+  });
 }
 
 export function format(ctx: VariablesContext, envs?: Variables): Variables {
@@ -30,7 +41,7 @@ export function format(ctx: VariablesContext, envs?: Variables): Variables {
   }
 
   for (const [name, value] of Object.entries(ctx.vars)) {
-    envs[validate(name)] = value;
+    envs[validate(name)] = interpolate(value, envs);
   }
 
   return envs;
--- test/vars.test.ts
+++ test/vars.test.ts
@@ -68,7 +68,7 @@ describe("vars", () => {
         expect(results).to.deep.equal({
           VAR_2: "envs second variable",
           VAR_5: "envs fifth variable",
-          });
+        });
       });
       it("creates an envs from a single level", () => {
         const ctx = new MockVarsContext({
@@ -82,7 +82,7 @@ describe("vars", () => {
           VAR_2: "second variable",
           VAR_3: "third variable",
           VAR_5: "envs fifth variable",
-          });
+        });
       });
       it("creates an envs from 2 levels", () => {
         const parent = new MockVarsContext({
@@ -104,12 +104,65 @@ describe("vars", () => {
         });
       });
     });
+    describe("interpolation", () => {
+      const envs = {
+        VAR_2: "envs second var",
+        VAR_5: "envs fifth var",
+      };
+
+      it("does simple replacement", () => {
+        const ctx = new MockVarsContext({
+          VAR_1: "interpolate ${VAR_2}",
+          VAR_3: "${VAR_5}",
+          VAR_4: "from '${VAR_10}'",
+        });
+        const results = format(ctx, envs);
+        expect(results).to.deep.equal({
+          VAR_1: "interpolate envs second var",
+          VAR_2: "envs second var",
+          VAR_3: "envs fifth var",
+          VAR_4: "from ''",
+          VAR_5: "envs fifth var",
+        });
+      });
+      it("replacement within siblings", () => {
+        const ctx = new MockVarsContext({
+          VAR_1: "static first var",
+          VAR_2: "carried '${VAR_1}'",
+          VAR_3: "carried '${VAR_4}'",
+          VAR_4: "static fourth var",
+        });
+        const results = format(ctx, envs);
+        expect(results).to.deep.equal({
+          VAR_1: "static first var",
+          VAR_2: "carried 'static first var'",
+          VAR_3: "carried ''",
+          VAR_4: "static fourth var",
+          VAR_5: "envs fifth var",
+          });
+      });
+
+      it("escapes the $", () => {
+        const ctx = new MockVarsContext({
+          VAR_1: "this is an escaped $$ sequence",
+          VAR_2: "$$",
+          VAR_3: "$${NOT_A_VAR}",
+        });
+        const results = format(ctx);
+        expect(results).to.deep.equal({
+          VAR_1: "this is an escaped $ sequence",
+          VAR_2: "$",
+          VAR_3: "${NOT_A_VAR}",
+        });
+      });
+    });
     describe("errors", () => {
       it("fails on an invalid var name", () => {
         const ctx = new MockVarsContext({
           "1_BAD_VAR": "bad variable",
         });
-        const err = expect(() => format(ctx)).to.throw(InvalidVariableName).actual;
+        const err =
+          expect(() => format(ctx)).to.throw(InvalidVariableName).actual;
         expect(err.varname).to.equal("1_BAD_VAR");
       });
     });
