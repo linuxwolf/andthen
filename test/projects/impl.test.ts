2f12b24153b29aacd24a6060dfc0497832697874
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-03 15:00:20 -0700

    test path

--- test/projects/impl.test.ts
+++ test/projects/impl.test.ts
@@ -60,6 +60,91 @@ describe("projects/impl", () => {
           ],
         });
       });
+      it("constructs from minimal config + parent", () => {
+        const parent = new Project({
+          name: "parent-project",
+        });
+        const project = new Project({
+          name: "my-project",
+        }, parent);
+
+        expect(project.name).to.equal("my-project");
+        expect(project.parent).to.equal(parent);
+        expect(project.root).to.be.false();
+        expect(project.default).to.equal("default");
+        expect(project.vars).to.deep.equal({});
+        expect(project.tasks).to.deep.equal({});
+
+        expect(project.toConfig()).to.deep.equal({
+          name: "my-project",
+        });
+      });
+      it("constructs from full config + parent", () => {
+        const parent = new Project({
+          name: "parent-project",
+        });
+        const project = new Project({
+          name: "my-project",
+          root: true,
+          default: "build-it",
+          vars: {
+            VAR_1: "project var one",
+          },
+          tasks: [
+            { name: "task-name" },
+          ],
+        }, parent);
+
+        expect(project.name).to.equal("my-project");
+        expect(project.parent).to.equal(parent);
+        expect(project.root).to.be.true();
+        expect(project.default).to.equal("build-it");
+        expect(project.vars).to.deep.equal({
+          VAR_1: "project var one",
+        });
+        expect(project.tasks).to.deep.equal({
+          "task-name": { name: "task-name" },
+        });
+
+        expect(project.toConfig()).to.deep.equal({
+          name: "my-project",
+          root: true,
+          default: "build-it",
+          vars: {
+            VAR_1: "project var one",
+          },
+          tasks: [
+            { name: "task-name" },
+          ],
+        });
+      });
+    });
+    describe("path()", () => {
+      it("returns a single-level", () => {
+        const project = new Project({
+          name: "my-project",
+        });
+
+        expect(project.path()).to.equal("my-project");
+      });
+      it("returns for a hierarchy", () => {
+        const parent = (() => {
+          const root = new Project({
+            name: "project-root",
+            root: true,
+          });
+          const sub = new Project({
+            name: "sub-project",
+          }, root);
+
+          return sub;
+        })();
+        const project = new Project({
+          name: "my-project",
+        }, parent);
+
+        expect(project.path()).to.equal("project-root/sub-project/my-project");
+      });
     });
   });
 });
