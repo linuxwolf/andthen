a6edf8eef60e08166af7ae8ce2d583c6709d6c17
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-03 13:37:57 -0700

    start on testing

---
+++ test/projects/impl.test.ts
@@ -0,0 +1,12 @@
+/** */
+
+import { describe, it } from "deno_std/testing/bdd.ts";
+import { expect } from "expecto/index.ts";
+
+import { Project } from "../../src/projects/impl.ts";
+
+describe("projects/impl", () => {
+  describe("Project", () => {
+
+  });
+});
