f6fe580f766bd8797a27bd5531b2fef71657a62b
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-10 11:06:44 -0700

    feat(task): schema validation (#23)

--- src/tasks/config.ts
+++ src/tasks/config.ts
@@ -1,13 +1,44 @@
 /** */
 
-import { ActionConfig } from "../actions/config.ts";
-import { Variables } from "../vars.ts";
+import { z } from "zod";
 
-export interface TaskConfig {
+import {
+  asConfig as asActionConfig,
+  Schema as ActionSchema,
+} from "../actions/config.ts";
+
+export const TaskSchema = z.object({
+  desc: z.string().optional(),
+  internal: z.boolean().optional(),
+  vars: z.record(z.string()).optional(),
+  deps: z.string().array().optional(),
+  steps: ActionSchema.array().transform((val) => (
+    val.map((s) => asActionConfig(s))
+  )).optional(),
+});
+
+export interface TaskConfig extends z.infer<typeof TaskSchema> {
   readonly name: string;
-  readonly desc?: string;
-  readonly internal?: boolean;
-  readonly vars?: Variables;
-  readonly deps?: string[];
-  readonly steps?: ActionConfig[];
+}
+
+export function asConfig(name: string, input: unknown): TaskConfig {
+  const data = TaskSchema.parse(input);
+
+  // extract simple fields
+  const {
+    desc,
+    internal,
+    vars,
+    deps,
+    steps,
+  } = data;
+
+  return {
+    name,
+    ...(desc && { desc }),
+    ...(internal && { internal }),
+    ...((Object.entries(vars || {}).length > 0) && { vars }),
+    ...(((deps || []).length > 0) && { deps }),
+    ...(((steps || []).length > 0) && { steps }),
+  } as TaskConfig;
 }
---
+++ test/tasks/config.test.ts
@@ -0,0 +1,113 @@
+/** */
+
+import { describe, it } from "deno_std/testing/bdd.ts";
+import { expect } from "expecto/index.ts";
+
+import { z } from "zod";
+import { asConfig } from "../../src/tasks/config.ts";
+
+describe("tasks/config", () => {
+  describe("asConfig()", () => {
+    describe("successes", () => {
+      it("returns from minimal", () => {
+        const config = asConfig("task-name", {});
+        expect(config).to.deep.equal({
+          name: "task-name",
+        });
+      });
+      it("returns from all simple", () => {
+        const config = asConfig("task-name", {
+          desc: "a test task",
+          internal: true,
+          vars: {
+            VAR_1: "task variable one",
+          },
+          deps: [
+            ":task-dep-1",
+          ],
+        });
+        expect(config).to.deep.equal({
+          name: "task-name",
+          desc: "a test task",
+          internal: true,
+          vars: {
+            VAR_1: "task variable one",
+          },
+          deps: [
+            ":task-dep-1",
+          ],
+        });
+      });
+      it("returns from just steps", () => {
+        const config = asConfig("task-name", {
+          steps: [
+            "do simple cmd",
+            { shell: "do shell command" },
+            { task: ":task-cmd" },
+          ],
+        });
+        expect(config).to.deep.equal({
+          name: "task-name",
+          steps: [
+            { shell: "do simple cmd" },
+            { shell: "do shell command" },
+            { task: ":task-cmd" },
+          ],
+        });
+      });
+      it("returns from full", () => {
+        const config = asConfig("task-name", {
+          desc: "a test task",
+          internal: true,
+          vars: {
+            VAR_1: "task variable one",
+          },
+          deps: [
+            ":task-dep-1",
+          ],
+          steps: [
+            "do simple cmd",
+            { shell: "do shell command" },
+            { task: ":task-cmd" },
+          ],
+        });
+        expect(config).to.deep.equal({
+          name: "task-name",
+          desc: "a test task",
+          internal: true,
+          vars: {
+            VAR_1: "task variable one",
+          },
+          deps: [
+            ":task-dep-1",
+          ],
+          steps: [
+            { shell: "do simple cmd" },
+            { shell: "do shell command" },
+            { task: ":task-cmd" },
+          ],
+        });
+      });
+    });
+
+    describe("errors", () => {
+      it("throws on simple invalid schema", () => {
+        expect(() => {
+          asConfig("task-name", {
+            internal: "stuff",
+            vars: ["some", "bad", "vars"],
+          });
+        }).to.throw(z.ZodError);
+      });
+      it("throws on steps invalid schema", () => {
+        expect(() => {
+          asConfig("task-name", {
+            steps: [
+              { stuff: "things" },
+            ],
+          });
+        }).to.throw(z.ZodError);
+      });
+    });
+  });
+});
