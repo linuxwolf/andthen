935237c03421514b79defe259ae2f9da18cb5d31
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-10 15:05:37 -0700

    project tasks short-syntax

--- src/projects/config.ts
+++ src/projects/config.ts
@@ -3,12 +3,20 @@
 import { z } from "zod";
 
 import { asConfig as asTaskConfig, TaskSchema } from "../tasks/config.ts";
+import { ActionSchema } from "../actions/config.ts";
+
+const TaskShortSchema = z.union([
+  TaskSchema,
+  ActionSchema.array().transform((steps) => ({
+    steps,
+  } as z.infer<typeof TaskSchema>)),
+])
 
 export const ProjectSchema = z.object({
   desc: z.string().optional(),
   root: z.boolean().optional(),
   vars: z.record(z.string()).optional(),
-  tasks: z.record(TaskSchema).transform((val) => (
+  tasks: z.record(TaskShortSchema).transform((val) => (
     Object.entries(val).map(([name, task]) => asTaskConfig(name, task))
   )).optional(),
 });
