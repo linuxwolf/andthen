ff596b71bcb94e65a406df2ea05b34d8bbc0c493
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-12-10 11:22:43 -0700

    initial project schema

--- src/projects/config.ts
+++ src/projects/config.ts
@@ -1,12 +1,35 @@
 /** */
 
-import { TaskConfig } from "../tasks/config.ts";
-import { Variables } from "../vars.ts";
+import { z } from "zod";
 
-export interface ProjectConfig {
+import { TaskSchema, asConfig as asTaskConfig } from "../tasks/config.ts";
+
+export const ProjectSchema = z.object({
+  root: z.boolean().optional(),
+  vars: z.record(z.string()).optional(),
+  tasks: z.record(TaskSchema).transform((val) => (
+    Object.entries(val).map(([name, task]) => asTaskConfig(name, task))
+  )).optional(),
+});
+
+export interface ProjectConfig extends z.infer<typeof ProjectSchema> {
   readonly name: string;
-  readonly default?: string;
-  readonly root?: boolean;
-  readonly vars?: Variables;
-  readonly tasks?: TaskConfig[];
+}
+
+export function asConfig(name: string, input: unknown): ProjectConfig {
+  const data = ProjectSchema.parse(input);
+
+  const {
+    root,
+    vars,
+    tasks,
+  } = data;
+
+  return {
+    name,
+    ...(root && { root }),
+    ...(data.default && { default: data.default }),
+    ...((Object.entries(vars || {}).length > 0) && { vars }),
+    ...(((tasks || []).length > 0) && { tasks }),
+  }
 }
