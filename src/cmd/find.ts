f761cefeecd48e5ddb4fc6207358171e0ae84eeb
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-06-06 17:20:58 -0600

    lint + fmt

--- src/cmd/find.ts
+++ src/cmd/find.ts
@@ -2,9 +2,17 @@ import { Command } from "@cliffy/command";
 
 export function command() {
   const cmd = new Command()
-    .option("--task-name, -t <string>", "task name to filter on", { collect: true })
-    .option("--depends-on, -d <string>", "project path to depend on", { collect: true })
-    .option("--depends-on-file <string>", "read `depends-on` paths from a file", { collect: true })
+    .option("--task-name, -t <string>", "task name to filter on", {
+      collect: true,
+    })
+    .option("--depends-on, -d <string>", "project path to depend on", {
+      collect: true,
+    })
+    .option(
+      "--depends-on-file <string>",
+      "read `depends-on` paths from a file",
+      { collect: true },
+    )
     .option("--depends-on-stdin", "read `depends-on` paths from `stdin`");
 
   return cmd;
