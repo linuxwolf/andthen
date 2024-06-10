f9b9b4545bb6bc6ac269c51372e3d5064e9f3c91
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-06-10 17:36:33 -0600

    Locator class

--- src/internal/project/locator.ts
+++ src/internal/project/locator.ts
@@ -45,3 +45,77 @@ export async function locate(path: string) {
   }
   return parseProject(prev, data);
 }
+
+export class Locator {
+  #inited = false;
+  #execDir: string;
+  #rootDir?: string;
+
+  #rootProject?: ProjectConfig;
+  #cache: Record<string, ProjectConfig> = {};
+
+  constructor(execDir: string) {
+    this.#execDir = execDir;
+  }
+
+  get execDir() {
+    return this.#execDir;
+  }
+
+  get rootDir() {
+    return this.#rootDir!;
+  }
+
+  get initialized() {
+    return this.#inited;
+  }
+
+  async init() {
+    const root = await this.#findRoot();
+    this.#rewire(root.path);
+
+    this.#inited = true;
+  }
+
+  async #findRoot() {
+    let found: ProjectConfig | undefined;
+    let currPath = this.#execDir;
+    let prevPath = currPath;
+
+    do {
+      const config = await locate(currPath);
+      if (!config) {
+        break;
+      }
+
+      found = config;
+      this.#cache[found.path] = config;
+      prevPath = found.path;
+      currPath = dirname(prevPath);
+    } while(!(found?.root));
+
+    if (!found) {
+      throw new Error("no root found");
+    }
+    return found;
+  }
+
+  #rewire(rootPath: string) {
+    const cache = this.#cache;
+    const result: typeof cache = {};
+
+    for (const k of Object.keys(cache)) {
+      const path = "//" + relative(rootPath, k);
+      const root = (path === "//");
+      result[path] = {
+        ...cache[k],
+        path,
+        root,
+      };
+    }
+
+    this.#cache = result;
+    this.#rootDir = rootPath;
+    this.#rootProject = this.#cache["//"];
+  }
+}
--- test/internal/project/locator.test.ts
+++ test/internal/project/locator.test.ts
@@ -1,46 +1,78 @@
 import { afterEach, beforeEach, describe, it } from "@std/testing/bdd";
 import { expect, mock } from "../../setup.ts";
 
-import { _internals, locate } from "../../../src/internal/project/locator.ts";
+import { stringify } from "@std/yaml";
+import { _internals, locate, Locator } from "../../../src/internal/project/locator.ts";
+
+const BASE_YAML = {
+  tasks: {
+    ":build": {},
+    ":test": {},
+    ":doc": {},
+  },
+}
 
 function stubReadTextFile(path: string | URL): Promise<string> {
   switch (path.toString()) {
-    case "/root/sub-project/andthen.yaml":
-      return Promise.resolve(`
-tasks:
-  :build: {}
-  :test: {}
-  :doc: {}
-`);
-    case "/root/andthen.yml":
-      return Promise.resolve(`
-root: true
-tasks:
-  :build: {}
-  :test: {}
-  :doc: {}    
-`);
+    case "/src/root/andthen.yml":
+      return Promise.resolve(stringify({
+        root: true,
+        ...BASE_YAML,
+      }));
+    case "/src/root/sub-project-1/andthen.yaml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+    case "/src/root/sub-project-1/sub-a/.andthen.yml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+      case "/src/root/sub-project-2/.andthen.yaml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+    
+    // implied root projects
+    case "/src/project/andthen.yaml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+    case "/src/project/sub-project-1/andthen.yaml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+    case "/src/project/sub-project-1/sub-a/.andthen.yml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+      case "/src/project/sub-project-2/.andthen.yaml":
+      return Promise.resolve(stringify({
+        ...BASE_YAML,
+      }));
+    
+
     default:
       return Promise.reject(new Deno.errors.NotFound());
   }
 }
 
 describe("internal/locator", () => {
+  let spyReadTextFile: mock.Spy;
+
+  beforeEach(() => {
+    spyReadTextFile = mock.stub(_internals, "readTextFile", stubReadTextFile);
+  });
+
+  afterEach(() => {
+    spyReadTextFile.restore();
+  });
+
   describe("locate()", () => {
-    let spyReadTextFile: mock.Spy;
-
-    beforeEach(() => {
-      spyReadTextFile = mock.stub(_internals, "readTextFile", stubReadTextFile);
-    });
-
-    afterEach(() => {
-      spyReadTextFile.restore();
-    });
-
     it("returns a non-root ProjectConfig", async () => {
-      const result = await locate("/root/sub-project");
+      const path = "/src/root/sub-project-1";
+      const result = await locate(path);
       expect(result).to.deep.equal({
-        path: "/root/sub-project",
+        path,
         root: false,
         defaults: {
           task: ":default",
@@ -53,10 +85,26 @@ describe("internal/locator", () => {
       });
     });
     it("returns a root ProjectConfig", async () => {
-      const result = await locate("/root");
+      const path = "/src/root";
+      const result = await locate(path);
       expect(result).to.deep.equal({
-        path: "/root",
+        path,
         root: true,
+        defaults: {
+          task: ":default",
+        },
+        tasks: {
+          ":build": {},
+          ":test": {},
+          ":doc": {},
+        },
+      });
+    });
+    it("returns the nearest ProjectConfig", async () => {
+      const result = await locate("/src/root/sub-project-2/sub-a");
+      expect(result).to.deep.equal({
+        path: "/src/root/sub-project-2",
+        root: false,
         defaults: {
           task: ":default",
         },
@@ -67,9 +115,46 @@ describe("internal/locator", () => {
         },
       })
     });
+
     it("return undefined when not found", async () => {
-      const result = await locate("/elsewhere/project");
+      const result = await locate("/app/project");
       expect(result).to.be.undefined();
+    });
+  });
+
+  describe("Locator", () => {
+    describe("ctor", () => {
+      it("constructs with the given execDir", () => {
+        const result = new Locator("/src/root");
+        expect(result.execDir).to.equal("/src/root");
+        expect(result.initialized).to.be.false();
+      });
+    });
+    describe("init()", () => {
+      it("initializes from the root directory", async () => {
+        const locator = new Locator("/src/root");
+        await locator.init();
+        expect(locator.initialized).to.be.true();
+        expect(locator.rootDir).to.equal("/src/root");
+      });
+      it("initializes from a sub-directory", async () => {
+        const locator = new Locator("/src/root/sub-project-1/sub-a");
+        await locator.init();
+        expect(locator.initialized).to.be.true();
+        expect(locator.rootDir).to.equal("/src/root");
+      });
+      it("initializes with an implied root", async () => {
+        const locator = new Locator("/src/project/sub-project-1/sub-a");
+        await locator.init();
+        expect(locator.initialized).to.be.true();
+        expect(locator.rootDir).to.equal("/src/project");
+      });
+
+      it("fails if no projects are found", async () => {
+        const locator = new Locator("/src/app/project-1");
+        const err = (await expect(locator.init()).to.be.rejected()).actual;
+        expect(err.message).to.equal("no root found");
+      });
     });
   });
 });
