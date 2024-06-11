5a40a3c672b1ba1627c912aa37ecdb1bb0ecba8c
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-06-11 17:05:29 -0600

    chore: custom errors (#7)

---
+++ src/internal/errors.ts
@@ -0,0 +1,29 @@
+export function format(msg: string, details?: Record<string, unknown>) {
+  const extra: string[] = [];
+
+  for (const [key, value] of Object.entries(details || {})) {
+    const str = Deno.inspect(value);
+    extra.push(`${key}: ${str}`);
+  }
+
+  if (extra.length > 0) {
+    return `${msg}: ( ${extra.join(", ")} )`;
+  }
+  return msg;
+}
+
+export abstract class ErrorBase extends Error {
+  constructor(msg: string, details?: Record<string, unknown>) {
+    super(format(msg, details));
+    this.name = this.constructor.name;
+  }
+}
+
+export class ProjectNotFound extends ErrorBase {
+  readonly path: string;
+
+  constructor(path: string, msg = "no project found") {
+    super(msg, { path });
+    this.path = path;
+  }
+}
---
+++ test/internal/errors.test.ts
@@ -0,0 +1,67 @@
+import { describe, it } from "@std/testing/bdd";
+import { expect } from "../setup.ts";
+
+import * as errors from "../../src/internal/errors.ts";
+
+describe("internal/errors", () => {
+  describe("format()", () => {
+    it("returns the message when no details", () => {
+      const result = errors.format("some error message");
+      expect(result).to.equal("some error message");
+    });
+    it("returns a formatted string with simple details", () => {
+      const timestamp = new Date();
+      const result = errors.format("some error message", {
+        path: "/some/path",
+        timestamp,
+      });
+      expect(result).to.equal(
+        `some error message: ( path: "/some/path", timestamp: ${timestamp.toISOString()} )`,
+      );
+    });
+  });
+
+  describe("ErrorBase", () => {
+    class MockError extends errors.ErrorBase {}
+    it("creates an ErrorBase with no details", () => {
+      const result = new MockError("some mock error");
+      expect(result.name).to.equal("MockError");
+      expect(result.message).to.equal("some mock error");
+      expect(result.stack).to.be.typeOf("string")
+        .and.not.empty();
+    });
+    it("creates an ErrorBase with details", () => {
+      const timestamp = new Date();
+      const result = new MockError("some mock error", {
+        path: "/some/path",
+        timestamp,
+      });
+      expect(result.name).to.equal("MockError");
+      expect(result.message).to.equal(
+        `some mock error: ( path: \"/some/path\", timestamp: ${timestamp.toISOString()} )`,
+      );
+      expect(result.stack).to.be.typeOf("string")
+        .and.not.empty();
+    });
+  });
+
+  describe("ProjectNotFound", () => {
+    it("creates a ProjectNotFound with default message", () => {
+      const result = new errors.ProjectNotFound("/root/no/project");
+      expect(result.name).to.equal("ProjectNotFound");
+      expect(result.message).to.equal(
+        'no project found: ( path: "/root/no/project" )',
+      );
+    });
+    it("creates a ProjectNotFound with custom message", () => {
+      const result = new errors.ProjectNotFound(
+        "/root/no/project",
+        "could not find a project",
+      );
+      expect(result.name).to.equal("ProjectNotFound");
+      expect(result.message).to.equal(
+        'could not find a project: ( path: "/root/no/project" )',
+      );
+    });
+  });
+});
