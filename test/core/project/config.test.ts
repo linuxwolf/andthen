ae22f3d28a979ffb775204718f8e6f04531ce3a8
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-09-07 07:44:46 -0600

    initi project config

---
+++ test/core/project/config.test.ts
@@ -0,0 +1,79 @@
+/** */
+
+import { describe, it } from "@std/testing/bdd";
+import { expect } from "../../setup.ts";
+
+import { from } from "../../../src/core/project/config.ts";
+
+describe("core/project", () => {
+  describe("from()", () => {
+    it("parses a complete project config", () => {
+      const result = from("//server", {
+        root: true,
+        tasks: {
+          ":build": {
+            desc: "build some project",
+            internal: false,
+          },
+          ":compile": {
+            desc: "compile some thing",
+            internal: true,
+          },
+        },
+      });
+
+      expect(result).to.deep.equal({
+        path: "//server",
+        root: true,
+        tasks: {
+          "//server:build": {
+            name: ":build",
+            desc: "build some project",
+            internal: false,
+          },
+          "//server:compile": {
+            name: ":compile",
+            desc: "compile some thing",
+            internal: true,
+          },
+        },
+      });
+    });
+    it("parses a partial project", () => {
+      const result = from("//server", {
+        tasks: {
+          ":build": {
+            desc: "build some project",
+          },
+          ":compile": {},
+        },
+      });
+
+      expect(result).to.deep.equal({
+        path: "//server",
+        root: false,
+        tasks: {
+          "//server:build": {
+            name: ":build",
+            desc: "build some project",
+            internal: false,
+          },
+          "//server:compile": {
+            name: ":compile",
+            desc: "",
+            internal: false,
+          },
+        },
+      });
+    });
+    it("parses an empty project", () => {
+      const result = from("//server", {});
+
+      expect(result).to.deep.equal({
+        path: "//server",
+        root: false,
+        tasks: {},
+      });
+    });
+  });
+});
