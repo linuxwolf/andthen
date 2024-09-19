ce6562a3c0b063b17a8969eb417ebcdacec3cdde
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-09-19 07:49:09 -0600

    chore: improve coverage to `main()` (#9)

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@
  * @copyright Matthew A. Miller 2024
  */
 
+import type { Internals } from "./util/types.ts";
+
 import { Command } from "@cliffy/command";
 import pkg from "../deno.json" with { type: "json" };
+
+export const _internals: Internals = {
+  command,
+  runnit: import.meta.main,
+};
 
 export function command(): Command {
   const cmd = new Command()
@@ -16,10 +23,10 @@ export function command(): Command {
 }
 
 export async function main() {
-  const cmd = command();
+  if (!_internals.runnit) return;
+
+  const cmd = _internals.command();
   await cmd.parse(Deno.args);
 }
 
-if (import.meta.main) {
-  await main();
-}
+await main();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,11 @@
 /** */
 
-import { describe, it } from "@std/testing/bdd";
-import { expect } from "./setup.ts";
+import { afterEach, beforeEach, describe, it } from "@std/testing/bdd";
+import { expect, mock } from "./setup.ts";
 
+import { Command } from "@cliffy/command";
 import pkg from "../deno.json" with { type: "json" };
-import { command } from "../src/main.ts";
+import { _internals, command, main } from "../src/main.ts";
 
 describe("main", () => {
   describe("command()", () => {
@@ -12,6 +13,30 @@ describe("main", () => {
       const cmd = command();
       expect(cmd.getName()).to.equal(pkg.short_name);
       expect(cmd.getVersion()).to.equal(pkg.version);
+    });
+  });
+
+  describe("main()", () => {
+    let spyCommand: mock.Spy;
+    let spyParse: mock.Spy;
+
+    beforeEach(() => {
+      spyCommand = mock.spy(_internals, "command");
+      spyParse = mock.spy(Command.prototype, "parse");
+    });
+
+    afterEach(() => {
+      spyCommand.restore();
+      spyParse.restore();
+    });
+
+    it("executes main", async () => {
+      _internals.runnit = true;
+
+      await main();
+
+      expect(spyCommand).to.have.been.called();
+      expect(spyParse).to.have.been.called();
     });
   });
 });
